cd4fbcff55c6d69322fe624a1a849972f8e55db0
Thapa-Dipesh <dthapa@4682>
2024-09-30 15:11:28 +0545

    about page

---
+++ World-Atlas/src/components/UI/HeroSection.jsx
@@ -0,0 +1,29 @@
+// import { FaLongArrowAltRight } from "react-icons/fa";
+
+export const HeroSection = () => {
+  return (
+    <main className="hero-section main">
+      <div className="container grid grid-two-cols">
+        <div className="hero-content">
+          <h1 className="heading-xl">
+            Explore the World, One Country at a Time.
+          </h1>
+          <p className="pragraph">
+            Discover the history, culture and beauty of every nation. Sport,
+            search and filter through countries to find the details you need.
+          </p>
+          <button className="btn btn-darken btn-inline bg-white-box">
+            Start Exploring{" "}
+          </button>
+        </div>
+        <div className="hero-image">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/012/576/863/non_2x/world-tour-travel-illustration-vector.jpg"
+            alt="World Beauty"
+            className="banner-image"
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
