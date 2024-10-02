9c8033ce77850d26285ffeadbd5340f951b97c69
Thapa-Dipesh <dthapa@4682>
2024-10-02 12:40:37 +0545

    contact page

--- World-Atlas/src/pages/Contact.jsx
+++ World-Atlas/src/pages/Contact.jsx
@@ -1,4 +1,46 @@
 export const Contact = () => {
-    return <h1>Contact Page</h1>;
+  const handleFormSubmit = (formData) => {
+    console.log(formData.entries);
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
   };
-  
+  return (
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+
+      <div className="container contact-wrapper">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            required
+            autoComplete="off"
+            placeholder="Enter your name"
+            name="username"
+          />
+
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter your email"
+            name="email"
+            required
+            autoComplete="off"
+          />
+
+          <textarea
+            name="message"
+            rows="10"
+            className="form-control"
+            placeholder="Enter your message"
+            required
+            autoComplete="off"></textarea>
+
+          <button type="submit" value="send">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
