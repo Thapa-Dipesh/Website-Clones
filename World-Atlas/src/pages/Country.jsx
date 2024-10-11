edf2222aa1b531866add75d84482041e0d99dbd7
Thapa-Dipesh <dthapa@4682>
2024-10-11 14:50:56 +0545

    country

--- World-Atlas/src/pages/Country.jsx
+++ World-Atlas/src/pages/Country.jsx
@@ -1,4 +1,28 @@
+import { useState, useEffect, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+import { CountryCard } from "../components/Layout/CountryCard";
+import { Loader } from "../components/UI/Loader";
+
 export const Country = () => {
-    return <h1>Country Page</h1>;
-  };
-  
+  const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
+      setCountries(res.data);
+    });
+  }, []);
+
+  if (isPending) return <Loader />;
+
+  return (
+    <section className="country-section">
+      <ul className="grid grid-four-cols">
+        {countries.map((curCountry, index) => {
+          return <CountryCard country={curCountry} key={index} />;
+        })}
+      </ul>
+    </section>
+  );
+};
