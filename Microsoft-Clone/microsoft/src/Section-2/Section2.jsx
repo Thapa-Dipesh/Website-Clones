f7cb373c97a1c497d5c3195f5bfe725e4ec69301
Thapa-Dipesh <dthapa@4682>
2024-12-06 16:05:20 +0545

    section component

---
+++ Microsoft-Clone/microsoft/src/Section-2/Section2.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Section2.css'
+
+const Section2 = () => {
+  return (
+    <div className='container'>
+      <div className="section-2-container">
+        <div className="section-2-text">
+        <h1>Organize your life</h1>
+        <p>Keep your ideas and notes organized with OneNote.</p>
+        <button>Learn more</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Section2
+
