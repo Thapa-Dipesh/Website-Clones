0d1f316e1d32d4adda7590f75ad8ec5f6b6fb04c
Thapa-Dipesh <dthapa@4682>
2024-12-06 16:04:57 +0545

    image slideshow

---
+++ Microsoft-Clone/microsoft/src/Section-1/Controller.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Controller.css'
+
+const Controller = () => {
+  return (
+    <>
+    <div className="controller-container">
+    <a href="#">&#60;</a>
+    <span className='box'></span>
+    <span className='box'></span>
+    <a href="#">&#62;</a>
+    </div>
+    </>
+  )
+}
+
+export default Controller
