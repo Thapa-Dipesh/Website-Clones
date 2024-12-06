27daea13f25b3dde0dd09d087d223718b53b195a
Thapa-Dipesh <dthapa@4682>
2024-12-06 16:03:59 +0545

    components added

--- Microsoft-Clone/microsoft/src/App.jsx
+++ Microsoft-Clone/microsoft/src/App.jsx
@@ -1,11 +1,23 @@
+import Card from "./CardContainer/Card";
+import Card2 from "./CardContainer2/Card2";
 import Header from "./components/Header";
+import Footer from "./Footer/Footer";
+import Controller from "./Section-1/Controller";
 import Section from "./Section-1/Section";
+import Section2 from "./Section-2/Section2";
+import Links from "./SocialLinks/Links";
 
 function App() {
   return (
     <>
       <Header />
       <Section />
+      <Controller />
+      <Card />
+      <Section2 />
+      <Card2 /> 
+      <Links />
+      <Footer />
     </>
   );
 }
