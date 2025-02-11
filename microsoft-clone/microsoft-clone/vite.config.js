8591d53591522fe09f2d5b5d042789a593730d3f
Thapa-Dipesh <dthapa@4682>
2025-02-11 14:18:42 +0545

    deployment

--- microsoft-clone/microsoft-clone/vite.config.js
+++ microsoft-clone/microsoft-clone/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/microsoft-clone/',
+  build: {
+    outDir: 'build', // Change output directory to 'build'
+  },
 })
