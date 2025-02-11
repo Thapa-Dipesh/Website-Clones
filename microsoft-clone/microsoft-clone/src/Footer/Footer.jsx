aab47b49652291790f47c70ae4a32764d825818b
Thapa-Dipesh <dthapa@4682>
2025-02-11 13:45:24 +0545

    code refactored

--- microsoft-clone/microsoft-clone/src/Footer/Footer.jsx
+++ microsoft-clone/microsoft-clone/src/Footer/Footer.jsx
@@ -25,51 +25,49 @@ const Footer = () => {
             <li><a href="#">Microsoft Teams for Education</a></li>
             <li><a href="#">Microsoft 365 Education</a></li>
             <li><a href="#">Office Education</a></li>
+            <li><a href="#">Educator training and development</a></li>
             <li><a href="#">Deals for Students and Parents</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
+            <li><a href="#">Azure for students</a></li>
           </ul>
         </div>
 
         <div className="sub-link">
           <p> Business</p>
           <ul>
-            <li><a href="#">Microsoft in education</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
+            <li><a href="#">Microsoft Cloud</a></li>
+            <li><a href="#">Microsoft Security</a></li>
+            <li><a href="#">Azure</a></li>
+            <li><a href="#">Dynamics 365</a></li>
+            <li><a href="#">Microsoft 365</a></li>
+            <li><a href="#">Microsoft Advertising</a></li>
+            <li><a href="#">Microsoft 365 Copilot</a></li>
+            <li><a href="#">Microsoft Teams</a></li>
           </ul>
         </div>
 
         <div className="sub-link">
           <p> Developer & It</p>
           <ul>
-            <li><a href="#">Microsoft in education</a></li>
-            <li><a href="#">Devices for education</a></li>
-            <li><a href="#">Microsoft Teams for Education</a></li>
-            <li><a href="#">Microsoft 365 Education</a></li>
-            <li><a href="#">Office Education</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
+            <li><a href="#">Microsoft Developer</a></li>
+            <li><a href="#">Documentation</a></li>
+            <li><a href="#">Microsoft Learn</a></li>
+            <li><a href="#">Microsoft Tech Community</a></li>
+            <li><a href="#">Azure Marketplace</a></li>
+            <li><a href="#">AppSource</a></li>
+            <li><a href="#">Microsoft Power Platform</a></li>
+            <li><a href="#">Visual Studio</a></li>
           </ul>
         </div>
 
         <div className="sub-link">
           <p> Company</p>
           <ul>
-            <li><a href="#">Microsoft in education</a></li>
-            <li><a href="#">Microsoft in education</a></li>
-            <li><a href="#">Microsoft in education</a></li>
-            <li><a href="#">Devices for education</a></li>
-            <li><a href="#">Microsoft Teams for Education</a></li>
-            <li><a href="#">Microsoft 365 Education</a></li>
-            <li><a href="#">Office Education</a></li>
-            <li><a href="#">Deals for Students and Parents</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">About Microsoft</a></li>
+            <li><a href="#">Company news</a></li>
+            <li><a href="#">Privacy at Microsoft</a></li>
+            <li><a href="#">Investors</a></li>
+            <li><a href="#">Sustainability</a></li>
           </ul>
         </div>
         
--- microsoft-clone/microsoft-clone/src/SocialLinks/Links.jsx
+++ microsoft-clone/microsoft-clone/src/SocialLinks/Links.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import './Links.css'
+import React from "react";
+import "./Links.css";
 
 const Links = () => {
   return (
-    <div className='links'>
-        <div className='social-links container'>
+    <div className="links">
+      <div className="social-links container">
         <p>Follow Microsoft</p>
-        <a href="#"><img src="../src/assets/asset 23.avif" alt="facebook" /></a>
-        <a href="#"><img src="../src/assets/asset 24.webp" alt="twitter" /></a>
-        <a href="#"><img src="../src/assets/asset 25.avif" alt="linkedin" /></a>
+        <a href="#">
+          <img src="../src/assets/asset 23.avif" alt="facebook" />
+        </a>
+        <a href="#">
+          <img src="../src/assets/asset 24.webp" alt="twitter" />
+        </a>
+        <a href="#">
+          <img src="../src/assets/asset 25.avif" alt="linkedin" />
+        </a>
+      </div>
+      <div className="back-link">Back to top</div>
     </div>
-    <div className="back-link">Back to top</div>
-    </div>
-  )
-}
+  );
+};
 
-export default Links
+export default Links;
