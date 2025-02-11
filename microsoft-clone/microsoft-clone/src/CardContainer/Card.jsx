6968f74e6aa72cdf1c6261583fa24a66986bdff3
Thapa-Dipesh <dthapa@4682>
2025-02-11 13:20:09 +0545

    cards refactored

--- microsoft-clone/microsoft-clone/src/CardContainer/Card.jsx
+++ microsoft-clone/microsoft-clone/src/CardContainer/Card.jsx
@@ -2,70 +2,55 @@ import React from "react";
 import "./Card.css";
 
 const Card = () => {
+  const cardDetails = [
+    {
+      image: "../src/assets/asset 2.avif",
+      title: "Designed for life today—and tomorrow",
+      description:
+        "The next-generation of games. Your goals. Friends and family. Windows 11 was made to bring you closer to everything you love",
+      btn: "See if your PC is ready",
+    },
+    {
+      image: "../src/assets/asset 14.avif",
+      title: "Microsoft Edge",
+      description:
+        " World-class performance with more privacy, more productivity, and more value while you browse.",
+      btn: "Download now",
+    },
+    {
+      image: "../src/assets/asset 17.avif",
+      title: "Microsoft OneDrive",
+      description:
+        "Save your files and photos to OneDrive and access them from any device, anywhere.",
+      btn: "Learn more",
+    },
+    {
+      image: "../src/assets/asset 19.avif",
+      title: "Outlook for iOS and Android",
+      description: " Connect. Organize. Get things done.",
+      btn: "Download now",
+    },
+  ];
+
   return (
-    <div className="card-container container">
-      <div className="card">
-        <div className="card-img">
-          <img src="../src/assets/asset 2.avif" alt="" />
-        </div>
-        <div className="card-title">
-          <h1>Designed for life today—and tomorrow</h1>
-          <p>
-            The next-generation of games. Your goals. Friends and family.
-            Windows 11 was made to bring you closer to everything you love
-          </p>
-        </div>
-        <div className="card-btn">
-          <button>See if your PC is ready</button>
-        </div>
+    <>
+      <div className="card-container container">
+        {cardDetails.map((item, index) => (
+          <div key={index} className="card">
+            <div className="card-img">
+              <img src={item.image} alt="" />
+            </div>
+            <div className="card-title">
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+            </div>
+            <div className="card-btn">
+              <button>{item.btn}</button>
+            </div>
+          </div>
+        ))}
       </div>
-
-      <div className="card">
-        <div className="card-img">
-          <img src="../src/assets/asset 14.avif" alt="" />
-        </div>
-        <div className="card-title">
-          <h1>Microsoft Edge</h1>
-          <p>
-          World-class performance with more privacy, more productivity, and more value while you browse.</p>
-        </div>
-        <div className="card-btn">
-          <button>Download now</button>
-        </div>
-      </div>
-
-
-      <div className="card">
-        <div className="card-img">
-          <img src="../src/assets/asset 17.avif" alt="" />
-        </div>
-        <div className="card-title">
-          <h1>Microsoft OneDrive</h1>
-          <p>
-          Save your files and photos to OneDrive and access them from any device, anywhere.</p>
-        </div>
-        <div className="card-btn">
-          <button>Learn more</button>
-        </div>
-      </div>
-
-
-      <div className="card">
-        <div className="card-img">
-          <img src="../src/assets/asset 19.avif" alt="" />
-        </div>
-        <div className="card-title">
-          <h1>Outlook for iOS and Android</h1>
-          <p>
-          Connect. Organize. Get things done.</p>
-        </div>
-        <div className="card-btn">
-          <button>Download now</button>
-        </div>
-      </div>
-
-
-    </div>
+    </>
   );
 };
 
